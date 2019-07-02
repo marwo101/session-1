2f8e5f515840e5f351f11d866346db1af9f1164a
marwo101 <51884641+marwo101@users.noreply.github.com>
2019-07-02 18:31:46 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-void myfunction(int x,int y,int k){
-    for(int k=1;k<=x;k++){
+
+int main()
+{ int x,y,k,u;
+scanf("%i",&x);
+scanf("%i",&y);
+for(int k=1;k<=x;k++){
     if(k%y==0){
         k++;
     }
-        printf("%i ",k);
+    printf("%i ",k);
+
 }
- return 0;
-}
-int main()
-{int x,y,k;
-scanf("%i",&x);
-scanf("%i",&y);
-myfunction(x,y,k);
+
     return 0;
 }
